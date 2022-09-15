c483e248304436d2c8c18156ad39cd28130da57a
ThomasBezanson12 <113362875+ThomasBezanson12@users.noreply.github.com>
2022-09-15 12:06:44 -0500

    Update main.py
    3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def topFiveMovies(a, b, c, d, e):
-    return a + " " + b + " " + c + " " + d  +  ""  +  e
+    return a + " " + b + " " + c + " " + d  +  " "  +  e
 print(topFiveMovies("One Flew over Cuckoos Nest",
 " The Wolf of Wall Street", " Frank", " The Shining",
 " The Prestige"))
