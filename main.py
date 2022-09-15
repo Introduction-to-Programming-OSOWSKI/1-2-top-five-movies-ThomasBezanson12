914cad6949af28cce3e302810143aab2e7d3e620
ThomasBezanson12 <818384@mystma.org>
2022-09-15 10:56:53 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a +  b +  c +  d +  e
+print(topFiveMovies("One Flew over Cuckoos Nest",
+" The Wolf of Wall Street", " Frank", " The Shining",
+" The Prestige"))
+
